df66d6c421b6ea807ee4f97205f841c7537597f6
Taeyang Jin <sun@jupiterflow.com>
2021-12-29 11:00:51 +0900

    refactor: Dto 프로퍼티 변경

--- pple-assets-interfaces/src/main/kotlin/link/pple/assets/interfaces/api/donation/dto.kt
+++ pple-assets-interfaces/src/main/kotlin/link/pple/assets/interfaces/api/donation/dto.kt
@@ -1,6 +1,5 @@
 package link.pple.assets.interfaces.api.donation
 
-import link.pple.assets.configuration.jpa.requiredId
 import link.pple.assets.domain.donation.entity.DonationHistory
 import link.pple.assets.interfaces.api.EntityDto
 import link.pple.assets.interfaces.api.entityData
@@ -10,15 +9,15 @@ import link.pple.assets.interfaces.api.entityData
  */
 
 data class DonationHistoryDto(
-    val donationId: Long,
-    val donorAccountId: Long,
+    val donationUuid: String,
+    val donorAccountUuid: String,
     val step: String
 ) : EntityDto()
 
 // ===============================
 
 internal fun DonationHistory.toDto() = DonationHistoryDto(
-    donationId = donation.requiredId,
-    donorAccountId = donor.requiredId,
+    donationUuid = donation.uuid.toString(),
+    donorAccountUuid = donor.uuid.toString(),
     step = step.name
 ).entityData(this)
