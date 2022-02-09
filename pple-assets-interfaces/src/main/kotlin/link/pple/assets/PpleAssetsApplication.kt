06728ff23f93450ca07f3ed4ac69d8e38685618c
emibgo2 <emibgo2@naver.com>
2022-02-09 22:41:42 +0900

    feat: WebMvcConfig 추가 ( CORS error )

--- pple-assets-interfaces/src/main/kotlin/link/pple/assets/PpleAssetsApplication.kt
+++ pple-assets-interfaces/src/main/kotlin/link/pple/assets/PpleAssetsApplication.kt
@@ -3,24 +3,40 @@ package link.pple.assets
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class PpleAssetsApplication
-//    (
-//    private val smsClient: SmsClient
-//) {
-//
-//    @PostConstruct
-//    fun init() {
-//        smsClient.sendOne()
-//    }
-//}
+class PpleAssetsApplication (){
+
+
+    @Component
+    class WebMvcConfig : WebMvcConfigurer {
+
+        override fun addCorsMappings(registry: CorsRegistry) {
+            registry.addMapping("/api/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS)
+        }
+
+        companion object {
+            private const val MAX_AGE_SECS: Long = 3600
+        }
+    }
+}
 
 fun main(args: Array<String>) {
+
     runApplication<PpleAssetsApplication>(*args)
+
 }
+
 
 @RestController
 class HelloRestController {
---
+++ pple-assets-interfaces/src/main/kotlin/link/pple/assets/configuration/WebMvcConfig.kt
@@ -0,0 +1,22 @@
+package link.pple.assets.configuration
+
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Component
+class WebMvcConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(MAX_AGE_SECS)
+    }
+
+    companion object {
+        private const val MAX_AGE_SECS: Long = 3600
+    }
+}
